4ac845db91f3385d6b80183f2e0edd0f7374ad1e
BenErnst <benernst3@gmail.com>
2022-02-02 14:25:38 +0200

    touch-canvas

--- src/cmps/Canvas.jsx
+++ src/cmps/Canvas.jsx
@@ -1,7 +1,20 @@
-import { forwardRef } from 'react';
+import { forwardRef, useEffect } from 'react';
 
 export const Canvas = forwardRef((props, ref) => {
     const { onStartDrawing, onFinishDrawing, onDraw } = props;
+
+    useEffect(() => {
+        console.log(ref.current);
+        ref.current.addEventListener('touchstart', () => {
+            onStartDrawing();
+        });
+        ref.current.addEventListener('touchend', () => {
+            onFinishDrawing();
+        });
+        ref.current.addEventListener('touchmove', () => {
+            onDraw();
+        });
+    }, []);
 
     return (
         <canvas
@@ -9,6 +22,9 @@ export const Canvas = forwardRef((props, ref) => {
             onMouseUp={onFinishDrawing}
             onMouseMove={onDraw}
             ref={ref}
+            // touchstart={onStartDrawing}
+            // touchend={onFinishDrawing}
+            // touchmove={onDraw}
             style={{
                 width: `${window.innerWidth / 2}px`,
                 height: `${window.innerHeight / 2}px`,
